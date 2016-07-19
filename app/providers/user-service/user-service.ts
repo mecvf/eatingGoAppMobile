36fd1698139ad14fb1e683c0f75e00e4c6d20853
Nicolas Molina Monroy <nicolas.molina.monroy@gmail.com>
2016-07-19 10:05:10 -0500

    article

---
+++ app/providers/user-service/user-service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
+
+/*
+  Generated class for the UserService provider.
+
+  See https://angular.io/docs/ts/latest/guide/dependency-injection.html
+  for more info on providers and Angular 2 DI.
+*/
+@Injectable()
+export class UserService {
+  data: any;
+
+  constructor(private http: Http) {
+    this.data = null;
+  }
+
+  load() {
+    if (this.data) {
+      // already loaded data
+      return Promise.resolve(this.data);
+    }
+
+    // don't have the data yet
+    return new Promise(resolve => {
+      this.http.get('https://randomuser.me/api/?results=25')
+        .map(res => res.json())
+        .subscribe(data => {
+          this.data = data.results;
+          resolve(this.data);
+        });
+    });
+  }
+}
+
