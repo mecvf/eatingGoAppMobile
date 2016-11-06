295ea5f166e20b9a8bc3a7a18179609cc77e6235
Nicolas Molina Monroy <nicolas.molina.monroy@gmail.com>
2016-11-06 14:45:31 -0500

    Upgrade Ionic 2 RC 2

--- .tmp/app/app.component.js
+++ .tmp/app/app.component.js
@@ -1,12 +1,3 @@
-var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
-    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
-    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
-    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
-    return c > 3 && r && Object.defineProperty(target, key, r), r;
-};
-var __metadata = (this && this.__metadata) || function (k, v) {
-    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
-};
 import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar, Splashscreen } from 'ionic-native';
@@ -21,12 +12,15 @@ export var MyApp = (function () {
             Splashscreen.hide();
         });
     }
-    MyApp = __decorate([
-        Component({
-            template: "<ion-nav [root]=\"rootPage\"></ion-nav>"
-        }), 
-        __metadata('design:paramtypes', [Platform])
-    ], MyApp);
+    MyApp.decorators = [
+        { type: Component, args: [{
+                    template: "<ion-nav [root]=\"rootPage\"></ion-nav>"
+                },] },
+    ];
+    /** @nocollapse */
+    MyApp.ctorParameters = [
+        { type: Platform, },
+    ];
     return MyApp;
 }());
 //# sourceMappingURL=app.component.js.map
--- .tmp/pages/home/home.js
+++ .tmp/pages/home/home.js
@@ -1,12 +1,3 @@
-var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
-    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
-    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
-    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
-    return c > 3 && r && Object.defineProperty(target, key, r), r;
-};
-var __metadata = (this && this.__metadata) || function (k, v) {
-    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
-};
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { UserService } from '../../providers/user-service';
@@ -21,13 +12,17 @@ export var HomePage = (function () {
             _this.users = data;
         });
     }
-    HomePage = __decorate([
-        Component({
-            selector: 'page-home',template:/*ion-inline-start:"/home/nicobytes/Work/projects/ion-book/demos/demo103/src/pages/home/home.html"*/'<ion-header>\n  <ion-navbar color="primary">\n    <ion-title>\n      Demo 103\n    </ion-title>\n  </ion-navbar>\n</ion-header>\n\n<ion-content>\n  <ion-list>\n    <ion-item *ngFor="let user of users">\n      <ion-avatar item-left>\n        <img [src]="user.picture.medium">\n      </ion-avatar>\n      <h2>{{ user.name.first | uppercase }}</h2>\n      <p>{{ user.email }}</p>\n    </ion-item>\n  </ion-list>\n</ion-content>\n'/*ion-inline-end:"/home/nicobytes/Work/projects/ion-book/demos/demo103/src/pages/home/home.html"*/,
-        }), 
-        __metadata('design:paramtypes', [(typeof (_a = typeof NavController !== 'undefined' && NavController) === 'function' && _a) || Object, (typeof (_b = typeof UserService !== 'undefined' && UserService) === 'function' && _b) || Object])
-    ], HomePage);
+    HomePage.decorators = [
+        { type: Component, args: [{
+                    selector: 'page-home',
+                    templateUrl: 'home.html',
+                },] },
+    ];
+    /** @nocollapse */
+    HomePage.ctorParameters = [
+        { type: NavController, },
+        { type: UserService, },
+    ];
     return HomePage;
-    var _a, _b;
 }());
 //# sourceMappingURL=home.js.map
